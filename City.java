a4c4009ea54f3d88425a24df7fcef0c84bfe0e1e
kartikkshirsagar <kartikpkshirsagar@gmail.com>
2020-01-26 16:02:08 +0530

    Made ride class and city class, defined costing

---
+++ City.java
@@ -0,0 +1,8 @@
+public class City{
+    int x_coordinate;
+    int y_coordinate;
+    City(int x , int y){
+        this.x_coordinate=x;
+        this.y_coordinate=y;
+    }
+}
---
+++ Ride.java
@@ -0,0 +1,13 @@
+
+public class Ride{
+    int rideCost;
+    Ride(City source,City destination)
+    {
+        this.rideCost=((source.x_coordinate - destination.x_coordinate) +(source.y_coordinate - destination.y_coordinate)) ;
+    }
+
+    Ride(City source,City destination,City via){
+        this.rideCost = ((source.x_coordinate - via.x_coordinate) +(source.y_coordinate - via.y_coordinate)) +((via.x_coordinate - destination.x_coordinate) +(via.y_coordinate - destination.y_coordinate));
+    }
+
+}
